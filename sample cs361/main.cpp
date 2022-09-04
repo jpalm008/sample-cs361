036328c0d21cbb2855632ce7cde1ddb095726ffa
Jerome Palmer <jpalmer008@odu.edu>
2022-09-03 21:35:34 -0400

    Initial Commit

---
+++ sample cs361/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sample cs361
+//
+//  Created by jerome palmer on 9/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
